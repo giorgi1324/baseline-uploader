8e6b943270571adb772cc0a513128f0a0441fa04
Giorgi Kvirkvia <kvirkvia@gmail.com>
2017-10-03 19:42:01 -0700

    Initial commit - sample app with status bar icon

---
+++ baseline-uploader/app.py
@@ -0,0 +1,48 @@
+import wx
+import wx.adv
+
+TRAY_TOOLTIP = 'System Tray Demo'
+TRAY_ICON = 'favicon.ico'
+
+
+def create_menu_item(menu, label, func):
+    item = wx.MenuItem(menu, -1, label)
+    menu.Bind(wx.EVT_MENU, func, id=item.GetId())
+    menu.Append(item)
+    return item
+
+
+class TaskBarIcon(wx.adv.TaskBarIcon):
+    def __init__(self):
+        super(TaskBarIcon, self).__init__()
+        self.set_icon(TRAY_ICON)
+        self.Bind(wx.adv.EVT_TASKBAR_LEFT_DOWN, self.on_left_down)
+
+    def CreatePopupMenu(self):
+        menu = wx.Menu()
+        create_menu_item(menu, 'Say Hello', self.on_hello)
+        menu.AppendSeparator()
+        create_menu_item(menu, 'Exit', self.on_exit)
+        return menu
+
+    def set_icon(self, path):
+        self.SetIcon(wx.Icon(path), TRAY_TOOLTIP)
+
+    def on_left_down(self, event):
+        print('Tray icon was left-clicked.')
+
+    def on_hello(self, event):
+        print('Hello, world!')
+
+    def on_exit(self, event):
+        wx.CallAfter(self.Destroy)
+
+
+def main():
+    app = wx.App(True)
+    TaskBarIcon()
+    app.MainLoop()
+
+
+if __name__ == '__main__':
+    main()
